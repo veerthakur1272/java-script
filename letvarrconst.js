ebab7380e806768441c5d3f80e8687a7fefc9358
veerthakur1272 <veerbhansinghthakur35@gmail.com>
2024-12-06 18:35:17 +0530

    addit

---
+++ letvarrconst.js
@@ -0,0 +1,39 @@
+// function veer (){
+//   var a=5;
+//   var b=10;
+//   var c=a+b
+//   console.log ("sum of a+b is:",c)
+
+
+// }
+// veer();
+
+function veer (){
+    // var a=5;
+    // var a=10;    //redeclare is allowed in var
+    // console.log(a)
+
+    // let a=5;
+    // let b=10;  //redeclare is not allowed in let
+    // console.log(a)
+
+    // const a=5;
+    // const b=10;
+    // console.log(a)  //redeclare is allowed const
+
+    // var a=5;
+    // b=10;    //reassingment is allowe in var
+    // console.log(a);
+
+    // let a=5;
+    // b=10;
+    // console.log(a);  //reassingment is allowed in let
+
+    // const a=5;
+    // b=10;  
+    // console.log(a)  //reassingment is allowed in let
+
+
+}
+
+veer();
---
+++ script.js
@@ -0,0 +1,5 @@
+ //var a=6;
+ //var b=9;
+ //console.log(a+b)
+
+ console.log(true==1) //true convert number (1)
